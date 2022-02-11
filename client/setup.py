bc34f8b644525f91c04a85e0e395d9a4c15bee2f
Oleksandr Korienev <alexkorienev@gmail.com>
2022-02-11 12:16:06 +0200

    Bump client to 0.3.2

--- client/setup.py
+++ client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evo-featureflags-client',
-    version='0.3.1',
+    version='0.3.2',
     description='Feature flags client',
     author='Vladimir Magamedov',
     author_email='vladimir@magamedov.com',
