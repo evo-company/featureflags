a4fd06062e409fe261f41261ae681253b6f45c47
m.kindritskiy <m.kindritskiy@smartweb.com.ua>
2023-07-18 16:37:18 +0300

    update docs

--- client/featureflags/client/flags.py
+++ client/featureflags/client/flags.py
@@ -225,3 +225,12 @@ class Client:
         finally:
             if tracer is not None:
                 self._manager.add_trace(tracer)
+
+    def preload(self, timeout=None):
+        """Preload flags from server.
+        This method syncs all flags with server"""
+        self._manager.preload(timeout=timeout, defaults=self._defaults)
+
+    async def preload_async(self, timeout=None):
+        """Async version of `preload` method"""
+        await self._manager.preload(timeout=timeout, defaults=self._defaults)
