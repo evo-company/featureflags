da4a8a57dcf846fc510b8db2b2620eec844ecb98
d.zakharchuk <d.zakharchuk@smartweb.com.ua>
2024-01-04 23:44:32 +0200

    fix imports in tests

--- featureflags/tests/state.py
+++ featureflags/tests/state.py
@@ -1,30 +1,46 @@
+import contextvars
 import inspect
-
-from uuid import uuid4
 from datetime import datetime, timedelta
+from uuid import uuid4
 
 import faker
 
-from sqlalchemy.engine import Row
-
-from featureflags.utils import sel_first
-from featureflags.models import Flag, Project, Variable
-from featureflags.models import VariableType, Check, Operator, Condition
-from featureflags.models import AuthSession, AuthUser, Changelog
-
+from featureflags.models import (
+    AuthSession,
+    AuthUser,
+    Changelog,
+    Check,
+    Condition,
+    Flag,
+    Operator,
+    Project,
+    Variable,
+    VariableType,
+)
+from featureflags.services.auth import BaseUserSession
+from featureflags.utils import select_first
 
 f = faker.Faker()
 
 
-async def _flush(db, model, data):
-    values = await sel_first(
-        db,
-        (
-            model.__table__.insert()
-            .values(data)
-            .returning(*model.__table__.columns)
-        ),
-    )
+def mk_session_var(
+    session: BaseUserSession,
+) -> contextvars.ContextVar[BaseUserSession]:
+    session_var = contextvars.ContextVar("session")
+    session_var.set(session)
+    return session_var
+
+
+async def _flush(db_engine, model, data):
+    async with db_engine.acquire() as conn:
+        values = await select_first(
+            conn,
+            (
+                model.__table__.insert()
+                .values(data)
+                .returning(*model.__table__.columns)
+            ),
+        )
     return values
 
 
@@ -35,84 +51,86 @@ def _val(value):
         return value
 
 
-async def mk_auth_user(db, *, id=uuid4, username=f.pystr):
+async def mk_auth_user(db_engine, *, id=uuid4, username=f.pystr):
     return await _flush(
-        db,
+        db_engine,
         AuthUser,
-        dict(
-            id=_val(id),
-            username=_val(username),
-        ),
+        {
+            "id": _val(id),
+            "username": _val(username),
+        },
     )
 
 
 async def mk_auth_session(
-    db,
+    db_engine,
     *,
     session=None,
     auth_user=None,
     creation_time=None,
     expiration_time=None,
 ):
-    auth_user = auth_user or await mk_auth_user(db)
+    auth_user = auth_user or await mk_auth_user(db_engine)
     return await _flush(
-        db,
+        db_engine,
         AuthSession,
-        dict(
-            session=session or uuid4().hex,
-            auth_user=auth_user.id,
-            creation_time=creation_time or datetime.utcnow(),
-            expiration_time=(
+        {
+            "session": session or uuid4().hex,
+            "auth_user": auth_user.id,
+            "creation_time": creation_time or datetime.utcnow(),
+            "expiration_time": (
                 expiration_time or datetime.utcnow() + timedelta(minutes=30)
             ),
-        ),
+        },
     )
 
 
-async def mk_project(db, *, id=uuid4, name=f.pystr, version=0):
+async def mk_project(db_engine, *, id=uuid4, name=f.pystr, version=0):
     return await _flush(
-        db,
+        db_engine,
         Project,
-        dict(
-            id=_val(id),
-            name=_val(name),
-            version=version,
-        ),
+        {
+            "id": _val(id),
+            "name": _val(name),
+            "version": version,
+        },
     )
 
 
 async def mk_variable(
-    db, *, id=uuid4, name=f.pystr, type=VariableType.STRING, project=None
+    db_engine, *, id=uuid4, name=f.pystr, type=VariableType.STRING, project=None
 ):
-    project = project or await mk_project(db)
+    project = project or await mk_project(db_engine)
     return await _flush(
-        db,
+        db_engine,
         Variable,
-        dict(
-            id=_val(id),
-            name=_val(name),
-            type=_val(type),
-            project=project.id,
-        ),
+        {
+            "id": _val(id),
+            "name": _val(name),
+            "type": _val(type),
+            "project": project.id,
+        },
     )
 
 
-async def mk_flag(db, *, id=uuid4, name=f.pystr, enabled=None, project=None):
-    project = project or await mk_project(db)
+async def mk_flag(
+    db_engine, *, id=uuid4, name=f.pystr, enabled=None, project=None
+):
+    project = project or await mk_project(db_engine)
     return await _flush(
-        db,
+        db_engine,
         Flag,
-        dict(
-            id=_val(id),
-            name=_val(name),
-            enabled=_val(enabled),
-            project=project.id,
-        ),
+        {
+            "id": _val(id),
+            "name": _val(name),
+            "enabled": _val(enabled),
+            "project": project.id,
+        },
     )
 
 
 async def mk_check(
-    db,
+    db_engine,
     *,
     id=uuid4,
     operator=Operator.EQUAL,
@@ -123,49 +141,55 @@ async def mk_check(
     value_set=None,
     variable_project=None,
 ):
-    variable = variable or await mk_variable(db, project=variable_project)
+    variable = variable or await mk_variable(
+        db_engine, project=variable_project
+    )
     return await _flush(
-        db,
+        db_engine,
         Check,
-        dict(
-            id=_val(id),
-            operator=_val(operator),
-            variable=variable.id,
-            value_string=_val(value_string),
-            value_number=_val(value_number),
-            value_timestamp=_val(value_timestamp),
-            value_set=_val(value_set),
-        ),
+        {
+            "id": _val(id),
+            "operator": _val(operator),
+            "variable": variable.id,
+            "value_string": _val(value_string),
+            "value_number": _val(value_number),
+            "value_timestamp": _val(value_timestamp),
+            "value_set": _val(value_set),
+        },
     )
 
 
-async def mk_condition(db, *, id=uuid4, flag=None, checks=None, project=None):
-    project = project or await mk_project(db)
-    flag = flag or await mk_flag(db, project=project)
-    checks = checks or [(await mk_check(db, variable_project=project)).id]
+async def mk_condition(
+    db_engine, *, id=uuid4, flag=None, checks=None, project=None
+):
+    project = project or await mk_project(db_engine)
+    flag = flag or await mk_flag(db_engine, project=project)
+    checks = checks or [
+        (await mk_check(db_engine, variable_project=project)).id
+    ]
     return await _flush(
-        db,
+        db_engine,
         Condition,
-        dict(
-            id=_val(id),
-            flag=flag.id,
-            checks=checks,
-        ),
+        {
+            "id": _val(id),
+            "flag": flag.id,
+            "checks": checks,
+        },
     )
 
 
 async def mk_changelog_entry(
-    db, *, timestamp=None, auth_user=None, flag=None, actions=tuple()
+    db_engine, *, timestamp=None, auth_user=None, flag=None, actions=()
 ):
-    auth_user = auth_user or await mk_auth_user(db)
-    flag = flag or await mk_flag(db)
+    auth_user = auth_user or await mk_auth_user(db_engine)
+    flag = flag or await mk_flag(db_engine)
     return await _flush(
-        db,
+        db_engine,
         Changelog,
-        dict(
-            timestamp=timestamp or datetime.utcnow(),
-            auth_user=auth_user.id,
-            flag=flag.id,
-            actions=actions,
-        ),
+        {
+            "timestamp": timestamp or datetime.utcnow(),
+            "auth_user": auth_user.id,
+            "flag": flag.id,
+            "actions": actions,
+        },
     )
