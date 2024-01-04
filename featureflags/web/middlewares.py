7a6e5d5264cbb5e3c3496af4b61df8a70dfa7d37
d.zakharchuk <d.zakharchuk@smartweb.com.ua>
2024-01-04 23:46:22 +0200

    replace sanic with fastapi, refactor web package

---
+++ featureflags/web/middlewares.py
@@ -0,0 +1,24 @@
+from collections.abc import Awaitable, Callable
+
+from fastapi import FastAPI, Request, Response
+
+from featureflags.container import Container
+from featureflags.services.auth import (
+    set_user_session_from_cookie,
+    set_user_session_to_response,
+)
+
+
+def configure_middlewares(app: FastAPI, container: Container) -> None:
+    @app.middleware("http")
+    async def set_user_session_and_auth_cookie(
+        request: Request,
+        call_next: Callable[[Request], Awaitable[Response]],
+    ) -> Response:
+        engine = await container.db_engine()
+        await set_user_session_from_cookie(request, engine)
+
+        response = await call_next(request)
+        await set_user_session_to_response(response)
+
+        return response
